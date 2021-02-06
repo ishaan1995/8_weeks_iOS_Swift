7ac75d1ee0661f0e23a037469b79c01d8bb9e52d
ishaan1995 <ishaankakkar95@gmail.com>
2021-02-06 19:24:41 +0530

    [init] hello world project

---
+++ 0.Basics/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by Ishaan Kakkar on 06/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
